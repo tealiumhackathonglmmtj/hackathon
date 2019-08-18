06221b765ec74bd0ab14d5c63a7b0e5b13463e76
John McCalley <jackmccalley@gmail.com>
2019-08-18 11:20:19 -0700

    displaying info

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,17 @@ const fetch = require("node-fetch")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tealium Hackathon' });
+  fetch('https://e2nkh9bvqg.execute-api.us-east-2.amazonaws.com/prod/visitorSampler?count=1', { title: 'Tealium Hackathon' })
+  .then((res)=>{
+    return res.json();
+  }).then((parsedResponse)=>{
+    console.log(parsedResponse);
+    var flags=[]
+    for (var property in parsedResponse[0].flags){
+      flags.push(property)
+    }
+    res.render('index', { title: 'Tealium Hackathon', user: parsedResponse, flags: flags})
+  })
 });
 
 router.get('/data', function(req, res, next) {
@@ -12,7 +22,12 @@ router.get('/data', function(req, res, next) {
   .then((res)=>{
     return res.json();
   }).then((parsedResponse)=>{
-    console.log(parsedResponse)
+    console.log(parsedResponse);
+    var flags=[]
+    for (var property in parsedResponse[0].flags){
+      flags.push(property)
+    }
+    res.render('index', { title: 'Tealium Hackathon', user: parsedResponse})
   })
 });
 
